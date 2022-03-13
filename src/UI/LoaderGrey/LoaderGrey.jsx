42c64eb705031dd0b52c2fe3e6c3dc60fd437e4a
evsem <frukwork@gmail.com>
2022-03-13 17:05:08 +0300

    v_2(UI)

---
+++ src/UI/LoaderGrey/LoaderGrey.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import classes from './LoaderGrey.module.css'
+
+const LoaderGrey = () => {
+  return <div className={classes.loaderGrey}></div>
+}
+
+export default LoaderGrey
